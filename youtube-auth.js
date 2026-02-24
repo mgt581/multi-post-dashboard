2b39e320a0974b701208219f279be6022543a890
mgt581 <alexbryantwork3234@outlook.com>
2026-02-24 22:48:10 +0000

    Update youtube-auth.js

--- youtube-auth.js
+++ youtube-auth.js
@@ -1,8 +1,11 @@
 // =============================
 // YouTube OAuth Config
 // =============================
-const YOUTUBE_CLIENT_ID = "PASTE_YOUR_CLIENT_ID_HERE";
-const YOUTUBE_REDIRECT_URI = "PASTE_YOUR_REDIRECT_URI_HERE";
+const YOUTUBE_CLIENT_ID =
+  "72559136598-p0tbkiiit42vuronhcigtdru23vcktvj.apps.googleusercontent.com";
+
+const YOUTUBE_REDIRECT_URI =
+  "https://multipostapp.co.uk/api/auth/callback/youtube";
 
 const YOUTUBE_SCOPE =
   "https://www.googleapis.com/auth/youtube.upload";
@@ -15,10 +18,12 @@ function connectYouTube() {
     "https://accounts.google.com/o/oauth2/v2/auth" +
     "?client_id=" + encodeURIComponent(YOUTUBE_CLIENT_ID) +
     "&redirect_uri=" + encodeURIComponent(YOUTUBE_REDIRECT_URI) +
-    "&response_type=token" +
+    "&response_type=code" +
     "&scope=" + encodeURIComponent(YOUTUBE_SCOPE) +
+    "&access_type=offline" +
     "&include_granted_scopes=true" +
-    "&state=youtube";
+    "&prompt=" + encodeURIComponent("consent select_account") +
+    "&state=" + encodeURIComponent("youtube");
 
   window.location.href = authUrl;
 }
